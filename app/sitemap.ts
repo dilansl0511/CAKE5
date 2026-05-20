8b4bc31ed0c8ec145c6251a974170bd2d7fb40d2
dilansl0511 <dilansl0511@gmail.com>
2026-05-20 13:57:34 -0600

    Actualización 12

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://caketoppercostarica.com",
-      lastModified: new Date(),
+      lastModified: new Date("2026-05-20"),
       changeFrequency: "weekly",
       priority: 1,
     },
